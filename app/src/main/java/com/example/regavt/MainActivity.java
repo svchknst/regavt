9e880e9d148463d6b9145729fe382a370a79a578
svchknst <nastyasavochkinan@gmail.com>
2023-06-03 09:03:54 +0300

    походу всё3

--- app/src/main/java/com/example/regavt/MainActivity.java
+++ app/src/main/java/com/example/regavt/MainActivity.java
@@ -1,14 +1,13 @@
-package com.example.reglog;
+package com.example.regavt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+
+import com.example.reglog.R;
 
 public class MainActivity extends AppCompatActivity {
     @Override
--- app/src/main/java/com/example/regavt/RegistrActivity.java
+++ app/src/main/java/com/example/regavt/RegistrActivity.java
@@ -1,4 +1,4 @@
-package com.example.reglog;
+package com.example.regavt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import com.example.reglog.R;
 
 public class RegistrActivity extends AppCompatActivity {
 
